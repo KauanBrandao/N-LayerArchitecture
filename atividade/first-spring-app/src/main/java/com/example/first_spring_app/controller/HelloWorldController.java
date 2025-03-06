e00c08d51816d4ded95233c8e5adc2213965fc98
Kauan <kauanbrandao07.com>
2025-03-06 14:03:15 -0300

    Atividade

---
+++ atividade/first-spring-app/src/main/java/com/example/first_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.first_spring_app.controller;
+
+import com.example.first_spring_app.service.HelloWorldService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    private HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return helloWorldService.helloWorld("Kauan");
+    }
+
+}
